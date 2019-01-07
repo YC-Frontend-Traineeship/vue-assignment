e1fb2ba02af9a266f3e05f34ae6df5d94ce3ba1a
Luisa Braga <luisasbraga@gmail.com>
2019-01-07 13:25:00 +0100

    layout changes

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,14 @@ Vue.component('barchart-soc', {
     props: ['soc'],
     template: '<canvas id="barchart-soc"></canvas>'
 })
+Vue.component('linechart-speed', {
+    props: ['speed'],
+    template: '<canvas id="linechart-speed"></canvas>'
+})
+Vue.component('linechart-soc', {
+    props: ['soc'],
+    template: '<canvas id="linechart-soc"></canvas>'
+})
 
 var dataTest = function () {
     // CREATE A NEW WEBSOCKETS CONNECTION
@@ -55,98 +63,102 @@ var app = new Vue({
 console.log(app.speed)
 
 
-    var ctx = document.getElementById("barchart-speed");
-    var chartSpeed = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: {
-            labels: [""],
-            datasets: [{
-                label: 'Current Speed',
-                data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)'
-                ],
-                borderWidth: 1
-            }],
-        }, 
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
+var ctx = document.getElementById("barchart-speed");
+var chartSpeed = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: [""],
+        datasets: [{
+            label: 'Current Speed',
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }],
+    }, 
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    // display: true,
+                    // padding: -4,
+                    // fontColor: "#000",
+                    // mirror: true
                     }
-                }]
-            },
-            legend: {
-                display: false
-            },
-            title: {
-                display: false
-            }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
         }
-    });
+    }
+});
 
-    var ctx = document.getElementById("barchart-soc");
-    var chartSOC = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: {
-            labels: [""],
-            datasets: [{
-                label: 'Speed 1',
-                data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)'
-                ],
-                borderWidth: 1
-            }],
-        }, 
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            },
-            legend: {
-                display: false
-            },
-            title: {
-                display: false
-            }
+var ctx = document.getElementById("barchart-soc");
+var chartSOC = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: [""],
+        datasets: [{
+            label: 'Speed 1',
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }],
+    }, 
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
         }
-    });
-    
-    function addData(chartSpeed, data) {
-        chartSpeed.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-            // console.log(data + " updated")
-        });
-        chartSpeed.update();
     }
-    
-    // function removeData(chartSpeed) {
-    //     // chartSpeed.data.datasets.forEach((dataset) => {
-    //         // console.log(dataset)
-    //         // addData(chartSpeed,dataObj.speed)
-    //         chartSpeed.datasets.destroy();
-    //         // console.log(data +" removed")
-    //     // });
-    //     chartSpeed.update();
-        
-    // }
+});
 
-    // Barchart - soc
-    function addDataSOC(chartSpeed, data) {
-        chartSpeed.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-            // console.log(data + " updated")
-        });
-        chartSpeed.update();
-    }
+function addData(chartSpeed, data) {
+    chartSpeed.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+        // console.log(data + " updated")
+    });
+    chartSpeed.update();
+}
+    
+// function removeData(chartSpeed) {
+//     // chartSpeed.data.datasets.forEach((dataset) => {
+//         // console.log(dataset)
+//         // addData(chartSpeed,dataObj.speed)
+//         chartSpeed.datasets.destroy();
+//         // console.log(data +" removed")
+//     // });
+//     chartSpeed.update();
+    
+// }
+
+// Barchart - soc
+function addDataSOC(chartSpeed, data) {
+    chartSpeed.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+        // console.log(data + " updated")
+    });
+    chartSpeed.update();
+}
