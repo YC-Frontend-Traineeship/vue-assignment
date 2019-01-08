5da611e4c680dee807a2b3798f5f3922f6a3d321
Luisa Braga <luisasbraga@gmail.com>
2019-01-08 17:26:41 +0100

    fix of commit errors

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,4 @@
+//  VUE COMPONENTS
 Vue.component('barchart-speed', {
     props: ['speed'],
     template: '<canvas id="barchart-speed"></canvas>'
@@ -6,13 +7,21 @@ Vue.component('barchart-soc', {
     props: ['soc'],
     template: '<canvas id="barchart-soc"></canvas>'
 })
+Vue.component('linechart-speed', {
+    props: ['speed'],
+    template: '<canvas id="linechart-speed"></canvas>'
+})
+Vue.component('linechart-soc', {
+    props: ['soc'],
+    template: '<canvas id="linechart-soc"></canvas>'
+})
 
-var dataTest = function () {
+
+var websocketData = function () {
     // CREATE A NEW WEBSOCKETS CONNECTION
     const url = 'ws://localhost:3000'
     const connection = new WebSocket(url)
     
-
     connection.onmessage = e => {
         dataObj = JSON.parse(e.data) 
         // console.log(dataObj) // All data from websocket
@@ -24,10 +33,10 @@ var dataTest = function () {
         app.odo = dataObj.odo
         newDataset = dataObj.speed
         
-        addData(chartSpeed, dataObj.speed)
-        // setTimeout(removeData(chartSpeed), 3000)
-        
-        addDataSOC(chartSOC, dataObj.soc)
+        updateBarchartSpeed(barchartSpeed, dataObj.speed)        
+        updateBarchartSOC(barchartSOC, dataObj.soc)
+        addDataSpeed(linechartSpeed, dataObj.speed)        
+        addDataSOC(linechartSOC, dataObj.soc)
     }
 
     connection.onerror = error => {
@@ -48,105 +57,164 @@ var app = new Vue({
     },
     mounted() {
     },
-    beforeMount: dataTest(),
+    beforeMount: websocketData(),
 });
 
-// console.log(app.speed)
-console.log(app.speed)
 
-
-    var ctx = document.getElementById("barchart-speed");
-    var chartSpeed = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: {
-            labels: [""],
-            datasets: [{
-                label: 'Current Speed',
-                data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)'
-                ],
-                borderWidth: 1
-            }],
-        }, 
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
+// BARCHART SPEED
+var ctx1 = document.getElementById("barchart-speed");
+var barchartSpeed = new Chart(ctx1, {
+    type: 'horizontalBar',
+    data: {
+        labels: [""],
+        datasets: [{
+            label: 'Current Speed',
+            data: [],
+            backgroundColor: [
+                '#7ca363'
+            ]
+        }],
+    }, 
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true,
                     }
-                }]
-            },
-            legend: {
-                display: false
-            },
-            title: {
-                display: false
-            }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
         }
-    });
+    }
+});
 
-    var ctx = document.getElementById("barchart-soc");
-    var chartSOC = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: {
-            labels: [""],
-            datasets: [{
-                label: 'Speed 1',
-                data: [],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)'
-                ],
-                borderWidth: 1
-            }],
-        }, 
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            },
-            legend: {
-                display: false
-            },
-            title: {
-                display: false
-            }
+function updateBarchartSpeed(barchartSpeed, data) {
+    barchartSpeed.data.datasets[0].data.shift()
+    barchartSpeed.data.datasets[0].data.push(data)
+    barchartSpeed.update();
+}
+
+
+// BARCHART STATE OF CHARGE
+var ctx2 = document.getElementById("barchart-soc");
+var barchartSOC = new Chart(ctx2, {
+    type: 'horizontalBar',
+    data: {
+        labels: [""],
+        datasets: [{
+            label: 'State of Charge',
+            data: [],
+            backgroundColor: [
+                '#7ca363'
+            ]
+        }],
+    }, 
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
         }
-    });
-    
-    function addData(chartSpeed, data) {
-        chartSpeed.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-            // console.log(data + " updated")
-        });
-        chartSpeed.update();
     }
-    
-    // function removeData(chartSpeed) {
-    //     // chartSpeed.data.datasets.forEach((dataset) => {
-    //         // console.log(dataset)
-    //         // addData(chartSpeed,dataObj.speed)
-    //         chartSpeed.datasets.destroy();
-    //         // console.log(data +" removed")
-    //     // });
-    //     chartSpeed.update();
-        
-    // }
+});
 
-    // Barchart - soc
-    function addDataSOC(chartSpeed, data) {
-        chartSpeed.data.datasets.forEach((dataset) => {
-            dataset.data.push(data);
-            // console.log(data + " updated")
-        });
-        chartSpeed.update();
+function updateBarchartSOC(barchartSOC, data) {
+    barchartSOC.data.datasets[0].data.shift()
+    barchartSOC.data.datasets[0].data.push(data)
+    barchartSOC.update();
+}
+
+
+// LINECHART SPEED
+var ctx3 = document.getElementById("linechart-speed");
+var linechartSpeed = new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: [""],
+        datasets: [{
+            label: 'Speed',
+            data: [],
+            borderColor: [
+                '#7ca363'
+            ]
+        },
+        {
+            label: 'Time',
+            data: [],
+            borderColor: [
+                '#000'
+            ]
+        }],
+    }, 
+    options: {
+        scales: {
+            yAxes: [{
+                stacked: true
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
+        }
     }
+});
+
+function addDataSpeed(linechartSpeed, data) {
+    linechartSpeed.data.datasets[0].data.unshift(data)
+    linechartSpeed.update();
+}
+
+// LINECHART STATE OF CHARGE
+var ctx4 = document.getElementById("linechart-soc");
+var linechartSOC = new Chart(ctx4, {
+    type: 'line',
+    data: {
+        labels: [""],
+        datasets: [{
+            label: 'State of Charge',
+            data: [],
+            borderColor: [
+                '#7ca363'
+            ]
+        },
+        {
+            label: 'Time',
+            data: [],
+            borderColor: [
+                '#000'
+            ]
+        }],
+    }, 
+    options: {
+        scales: {
+            yAxes: [{
+                stacked: true
+            }]
+        },
+        legend: {
+            display: false
+        },
+        title: {
+            display: false
+        }
+    }
+});
+
+function addDataSOC(linechartSOC, data) {
+    linechartSOC.data.datasets[0].data.unshift(data)
+    linechartSOC.update();
+}
